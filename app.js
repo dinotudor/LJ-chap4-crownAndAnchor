1cf35512af08c090cfde9c951b997ff02f32fc99
Dino Tudor <dinotudor@gmail.com>
2019-07-24 12:00:26 +0200

    pseudo code

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
-const funds = 50;
-let bets = {};
-let hand = [];
+/*
+BASIC CONDITIONS:
+
+Player initial stack, 50. If double stack, player quit. If Stack less then 100 gamble until his broke
+
+*/
+let funds = 50
+let bets = {}
+let hand = []
 
 function randomInteger() {
   Math.floor(Math.random() * 6)
@@ -9,3 +15,15 @@ function randomInteger() {
 function randFace () {
   console.log('random Face')
 }
+
+// Object property assignment
+bets['heart'] = 5
+bets[randFace()] = 5
+
+//Adding elements to an array
+hand.push(randFace())
+
+let arithimetics1 = funds - totalBet
+let arithimetics2 = funds + winnings
+
+// Flowchart decision path

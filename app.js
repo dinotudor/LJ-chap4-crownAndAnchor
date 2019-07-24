e53821fddfc19eab4ee8863b87ff65a02e58ec63
Dino Tudor <dinotudor@gmail.com>
2019-07-24 16:51:47 +0200

    finish control flow

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
-/*
-BASIC CONDITIONS:
-
-Player initial stack, 50. If double stack, player quit. If Stack less then 100 gamble until his broke
-
-*/
+//BASIC CONDITIONS:
+//Player initial stack, 50. If double stack, player quit. If Stack less then 100 gamble until his broke
 
 function rand(m, n) {
   return m + Math.floor((n - m +1) * Math.random());
@@ -15,15 +11,18 @@ function randFace () {
 console.log(randFace());
 
 let funds = 50;
-let hand = [];
-const bets = {
-  crown: 0,
-  anchor: 0,
-  heart: 0,
-  spade: 0,
-  club: 0,
-  diamond: 0,
-};
+let round = 0;
+
+while(funds > 1 && funds < 100) {
+  round++;
+  console.log(`round ${round}`);
+  console.log(`\tstarting funds: ${funds}p`)
+  //place bets
+  let bets = { crown: 0, anchor: 0, heart: 0, spade: 0, club: 0, diamond: 0 };
+  // roll dice
+
+  // collect winnings
+}
 
 let totalBet = rand(1, funds);
 console.log(totalBet);
@@ -31,20 +30,23 @@ if (totalBet === 7) {
   totalBet = funds;
   bets.heart = totalBet
 } else {
-  console.log(bets)
+  let remaining = totalBet;
+  do {
+    let bet = rand(1, remaining);
+    let face = randFace();
+    bets[face] = bets[face] + bet;
+    remaining -= bet
+  } while (remaining > 0)
 }
+funds -= totalBet;
+console.log('\tbets: ' + Object.keys(bets).map(face => `${face}: ${bets[face]} pence`).join(', ') + `(total: ${totalBet} pence`);
 
-let remaining = totalBet;
-do {
-  let bet = rand(1, remaining);
-  let face = randFace();
-  bets[face] = bets[face] + bet;
-  remaining -= bet
-} while (remaining > 0)
-
+//ROLL DICE
+const hand = [];
 for(let roll = 0; roll < 3; roll++) {
   hand.push(randFace());
 }
+console.log(`\thand: ${hand.join(', ')}`);
 
 let winnings = 0;
 for(let die = 0; die < hand.length; die++) {
@@ -52,13 +54,19 @@ for(let die = 0; die < hand.length; die++) {
   if(bets[face] > 0) winnings += bets[face];
 }
 funds += winnings
+console.log(`\twinning: ${winnings}`);
+console.log(`\tending fun: ${funds}`)
+
+
+
+
 
 // Object property assignment
-bets['heart'] = 5
-bets[randFace()] = 5
+//bets['heart'] = 5
+//bets[randFace()] = 5
 
 //Adding elements to an array
-hand.push(randFace())
+//hand.push(randFace())
 
 //let arithimetics1 = funds - totalBet
 //let arithimetics2 = funds + winnings
@@ -74,10 +82,3 @@ Logical Operators (funds > 0 && funds < 100)
 
 */
 
-while(funds > 1 && funds < 100) {
-  // place bets
-  funds += 2
-  // roll dice
-
-  // collect winnings
-}

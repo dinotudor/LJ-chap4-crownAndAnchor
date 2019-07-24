96775c741311abbe12d6143056541c9cd01d49b6
Dino Tudor <dinotudor@gmail.com>
2019-07-24 12:48:37 +0200

    helper functions

--- app.js
+++ app.js
@@ -4,17 +4,19 @@ BASIC CONDITIONS:
 Player initial stack, 50. If double stack, player quit. If Stack less then 100 gamble until his broke
 
 */
-let funds = 50
-let bets = {}
-let hand = []
+let funds = 50;
+let bets = {};
+let hand = [];
 
-function randomInteger() {
-  Math.floor(Math.random() * 6)
+function rand(m, n) {
+  return m + Math.floor((n - m +1) * Math.random());
 }
 
 function randFace () {
-  console.log('random Face')
+  return ["crown", "anchor", "heart", "spade", "club", "diamond"][rand(0,5)];
 }
+
+console.log(randFace());
 
 // Object property assignment
 bets['heart'] = 5
@@ -23,7 +25,24 @@ bets[randFace()] = 5
 //Adding elements to an array
 hand.push(randFace())
 
-let arithimetics1 = funds - totalBet
-let arithimetics2 = funds + winnings
+//let arithimetics1 = funds - totalBet
+//let arithimetics2 = funds + winnings
 
-// Flowchart decision path
+//let placeBet = roll++
+
+/*
+Flowchart decision path
+
+Numeric comparisons (funds > 0, funds < 100)
+Equality comparisons (totalBet === 7)
+Logical Operators (funds > 0 && funds < 100)
+
+*/
+
+while(funds > 1 && funds < 100) {
+  // place bets
+  funds += 2
+  // roll dice
+
+  // collect winnings
+}
